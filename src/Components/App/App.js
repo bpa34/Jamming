6f8bd4433d519946617b5ac8476f65b3e6c21e40
bpa34 <brett.atkinson@me.com>
2023-01-30 19:21:43 -0800

    Added Functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,6 +17,9 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track){
     let tracks = this.state.playlistTracks;
@@ -33,6 +36,20 @@ class App extends React.Component {
 
       this.setState({playlistTracks: tracks})
     }
+
+    updatePlaylistName(newName){
+      this.setState({
+        playlistName: newName
+      })
+    }
+
+    savePlaylist(){
+      const trackUris = this.state.playlistTracks.map(track => track.uri);
+    }
+
+    search(term){
+      console.log(term)
+    }
   
 
 
@@ -41,10 +58,17 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar/>
+          <SearchBar onSearch = {this.search}/>
           <div className="App-playlist">
-            <SearchResults searchResults = {this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName = {this.state.playlistName} playlistTracks = {this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <SearchResults searchResults = {this.state.searchResults} 
+                           onAdd={this.addTrack}
+                           />
+            <Playlist playlistName = {this.state.playlistName} 
+                      playlistTracks = {this.state.playlistTracks} 
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName}
+                      onSave = {this.savePlaylist}
+                      />
           </div>
         </div>
       </div>
